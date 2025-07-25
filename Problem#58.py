bf13c9cbc750c3b2b184e9c999b1d042600199d8
shahan360 <sbsb4961@gmail.com>
2025-07-24 19:49:52 -0500

    committed

---
+++ Problem#58.py
@@ -0,0 +1,18 @@
+'''
+Problem 102: Binary Tree Level Order Traversal
+Problem Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+'''
+
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this :
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
---
+++ Problem#59.py
@@ -0,0 +1,13 @@
+'''
+Prolem No. 207. Course Schedule
+Problem Link: https://leetcode.com/problems/course-schedule/description/
+'''
+
+# Time Complexity : 
+# Space Complexity : 
+# Did this code successfully run on Leetcode : 
+# Any problem you faced while coding this :
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
